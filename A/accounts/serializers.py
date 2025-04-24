bb917c1c3973b669cd6207eba67da11c510d4508
Erfan-aminian <aminian158@gmail.com>
2025-04-24 08:31:20 -0400

    Add custom validators

--- A/accounts/serializers.py
+++ A/accounts/serializers.py
@@ -12,4 +12,13 @@ class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
-    #password2 = serializers.CharField(required=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('username cant be admin')
+        return value
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Passwords must match')
+        return data
